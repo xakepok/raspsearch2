c0ab8fa81ee8c1e3bab9c9fb6d011e908d15682b
asharikov <xakepok@xakepok.com>
2019-07-24 11:54:09 +0300

    Form action

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,20 +1,15 @@
 <?php
-/**
- * @package    raspsearch
- *
- * @author     sharikov <your@email.com>
- * @copyright  A copyright
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * @link       http://your.url.com
- */
-
 defined('_JEXEC') or die;
+
+require_once JPATH_ADMINISTRATOR . '/components/com_rw/helpers/rw.php';
 
 // Access to module parameters
 $domain = $params->get('domain', 'https://www.joomla.org');
+$itemID = RwHelper::getMenuItemId('schedule');
+$action = JRoute::_("index.php?option=com_rw&amp;view=schedule&amp;Itemid={$itemID}");
 ?>
 
-<form action="#" method="get">
+<form action="<?php echo $action;?>" method="get">
     <div class="form-row">
         <div class="col-lg-5">
             <select name="from" id="rasp_from" class="form-control mt-1 mb-1" style="width:auto;">
@@ -27,7 +22,7 @@ $domain = $params->get('domain', 'https://www.joomla.org');
             </select>
         </div>
         <div class="col-lg-2">
-            <button class="btn btn-success mt-1 mb-1 w-100"><?php echo JText::sprintf('MOD_RASPSEARCH_BUTTON_SEARCH_TEXT');?></button>
+            <button class="btn btn-success mt-1 mb-1 w-100" type="submit"><?php echo JText::sprintf('MOD_RASPSEARCH_BUTTON_SEARCH_TEXT');?></button>
         </div>
     </div>
 </form>
